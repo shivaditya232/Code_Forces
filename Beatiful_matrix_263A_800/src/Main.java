a46d6ca38aba7a59103d8ad7ab1439f3d80365e3
shivaditya232 <shivaditya2503@gmail.com>
2026-07-03 19:22:21 +0530

    Add Beautiful Matrix 263A solution

---
+++ Beatiful_matrix_263A_800/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int[][] matrix = new int[5][5];
+        System.out.println("Enter the row you want to make 1");
+        int r= scanner.nextInt();
+        System.out.println("Enter the column you want to make 1");
+        int c= scanner.nextInt();
+        matrix[r][c]=1;
+        int count=0;
+        int col=0;
+        int row=0;
+        col=Math.abs(c-2);
+        row=Math.abs(r-2);
+        count=col+row;
+        System.out.println(count);
+    }
+}
