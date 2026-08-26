c758ce7cf501160ddd5547758be7db750417a8f3
shivaditya232 <shivaditya2503@gmail.com>
2026-08-26 21:49:18 +0530

    1 problem solved

---
+++ Minimum_LCM_1765M_1000/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long t;
+	cin >> t;
+	while (t--)
+	{
+		long long n;
+		cin >> n;
+		long long ans_a = 1;
+		long long ans_b = n - 1;
+		for (long long fac = 2; fac * fac <= n; fac++)
+		{
+			if (n % fac == 0)
+			{
+				ans_a = n / fac;
+				ans_b = n - ans_a;
+				break;
+			}
+		}
+		cout << ans_a << " " << ans_b << endl;
+	}
+	return 0;
+}
