e51ebeb72ce676a956d029e52b984a6d5dbd0ff3
shivaditya232 <shivaditya2503@gmail.com>
2026-09-10 22:47:14 +0530

    done

---
+++ Add_and_divid_1485A_1000/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		long long a, b;
+		cin >> a >> b;
+
+		long long ans = INT_MAX;
+		for (int addition = 0; addition < 32; addition++)
+		{
+			long long operations = addition;
+			long long new_b = b + addition;
+			if (new_b == 1)
+				continue;
+			long long copy_a = a;
+			while (copy_a > 0)
+			{
+				copy_a /= new_b;
+				operations++;
+			}
+			ans = min(ans, operations);
+		}
+		cout << ans << endl;
+	}
+}
